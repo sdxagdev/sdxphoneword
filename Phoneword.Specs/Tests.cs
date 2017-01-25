5b0f58057ec2088f421a7a3f816fc1043d213ed2
kosanovicbo <kosanovicbo@WKKOSANOVICBO01>
2017-01-25 18:13:02 +0100

    Removed reference to the Android project for the test cloud

--- Phoneword.Specs/Tests.cs
+++ Phoneword.Specs/Tests.cs
@@ -1,41 +1,41 @@
-﻿using System;
-using System.IO;
-using System.Linq;
-using NUnit.Framework;
-using TechTalk.SpecFlow;
-using Xamarin.UITest;
-using Xamarin.UITest.Queries;
+﻿//using System;
+//using System.IO;
+//using System.Linq;
+//using NUnit.Framework;
+//using TechTalk.SpecFlow;
+//using Xamarin.UITest;
+//using Xamarin.UITest.Queries;
 
-namespace Phoneword.Specs
-{
-    [TestFixture(Platform.Android)]
-    [TestFixture(Platform.iOS)]
-    public class Tests
-    {
-        IApp app;
-        Platform platform;
+//namespace Phoneword.Specs
+//{
+//    [TestFixture(Platform.Android)]
+//    [TestFixture(Platform.iOS)]
+//    public class Tests
+//    {
+//        IApp app;
+//        Platform platform;
 
-        public Tests(Platform platform)
-        {
-            this.platform = platform;
-        }
+//        public Tests(Platform platform)
+//        {
+//            this.platform = platform;
+//        }
 
-        [SetUp]
-        public void BeforeEachTest()
-        {
-            app = AppInitializer.StartApp(platform);
+//        [SetUp]
+//        public void BeforeEachTest()
+//        {
+//            app = AppInitializer.StartApp(platform);
 
-            if (!FeatureContext.Current.ContainsKey("App"))
-            {
-                FeatureContext.Current.Add("App", app);
-            }
-        }
+//            if (!FeatureContext.Current.ContainsKey("App"))
+//            {
+//                FeatureContext.Current.Add("App", app);
+//            }
+//        }
 
-        [Test]
-        public void AppLaunches()
-        {
-            app.Screenshot("First screen.");
-        }
-    }
-}
+//        [Test]
+//        public void AppLaunches()
+//        {
+//            app.Screenshot("First screen.");
+//        }
+//    }
+//}
 
